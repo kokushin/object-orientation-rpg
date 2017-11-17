560ddf9fb41da9555a5b3ebf9db2eb44b930ba58
kokushin <mail@kokush.in>
2017-11-17 10:25:39 +0900

    MP計算まで

--- character.js
+++ character.js
@@ -1,4 +1,5 @@
 class Character {
+
   constructor (data) {
     this.status = {
       name: data.name,
@@ -14,22 +15,23 @@ class Character {
   }
 
   init () {
-    this.displayStatus()
-    this.displaySkills()
+    this.showStatus()
+    this.showSkills()
   }
 
-  displayStatus () {
+  showStatus () {
     document.getElementById(this.status.type).insertAdjacentHTML('beforeend', `
       <p>${this.status.name} / Lv: ${this.status.lv} / Exp: ${this.status.exp}</p>
-      <p>HP: ${this.status.hp} / MP: ${this.status.mp}</p>
+      <p>HP: <span id="${this.status.type}-hp">${this.status.hp}</span> / MP: <span id="${this.status.type}-mp">${this.status.mp}</span></p>
     `)
   }
 
-  displaySkills () {
+  showSkills () {
     for (let i = 0; i < Object.keys(this.skills).length; i++) {
       document.getElementById(this.status.type).insertAdjacentHTML('beforeend', `
         <button data-skill-id="${i}" data-status-type="${this.status.type}" onclick="system.useSkill(this)">${this.skills[i].name}</button>
       `)
     }
   }
+
 }
--- system.js
+++ system.js
@@ -1,5 +1,10 @@
 class System {
+
   constructor (player, mob) {
+    this.phase = {
+      count: 1,
+      target: 'player'
+    }
     this.player = player
     this.mob = mob
 
@@ -7,30 +12,71 @@ class System {
   }
 
   init () {
-    this.start()
+    this.log(`${this.mob.status.name}[Lv${this.mob.status.lv}] が現れた！`)
+    this.log(`===== [Phase${this.phase.count}] Start =====`, 1000)
+    this.log(`${this.player.status.name} はどうする？`, 2000)
   }
 
-  start () {
-    this.log(`${this.mob.status.name} (Lv: ${this.mob.status.lv}) が現れた！`)
-  }
-
-  log (message) {
-    document.getElementById('log').insertAdjacentHTML('afterbegin', `
-      <li>${message}</li>
-    `)
+  log (message, timer = 0) {
+    setTimeout (() => {
+      document.getElementById('log').insertAdjacentHTML('afterbegin', `
+        <li>${message}</li>
+      `)
+    }, timer)
   }
 
   useSkill (e) {
     const id = Number(e.getAttribute('data-skill-id'))
     const type = e.getAttribute('data-status-type')
+    let skillName = ''
+    let skillType = ''
+    let skillTarget = ''
+    let skillMp = 0
+    let skillPower = {}
 
     switch (type) {
       case 'player':
-        console.log(this.player.skills[id].name)
+        skillName = this.player.skills[id].name
+        skillType = this.player.skills[id].type
+        skillTarget = this.player.skills[id].target
+        skillMp = this.player.skills[id].mp
+        skillPower = this.player.skills[id].power
+
+        this.mpCalculation(type, skillMp)
+
+        this.log(`${this.player.status.name} の ${skillName}！！`)
         break;
       case 'mob':
-        console.log(this.mob.skills[id].name)
+        skillName = this.mob.skills[id].name
+        skillType = this.mob.skills[id].type
+        skillTarget = this.mob.skills[id].target
+        skillMp = this.mob.skills[id].mp
+        skillPower = this.mob.skills[id].power
+
+        this.mpCalculation(type, skillMp)
+
+        this.log(`${this.mob.status.name} の ${skillName}！！`)
+        break;
+    }
+
+    this.damageCalculation(skillName, skillType, skillTarget, skillMp, skillPower)
+  }
+
+  damageCalculation (skillName, skillType, skillTarget, skillMp, skillPower) {
+    console.log(skillName, skillType, skillTarget, skillMp, skillPower)
+  }
+
+  mpCalculation (type, skillMp) {
+    switch (type) {
+      case 'player':
+        this.player.status.mp = this.player.status.mp - skillMp
+        document.getElementById('player-mp').innerHTML = this.player.status.mp
+        break;
+      case 'mob':
+        this.mob.status.mp = this.mob.status.mp - skillMp
+        document.getElementById('mob-mp').innerHTML = this.mob.status.mp
         break;
     }
   }
+
 }
